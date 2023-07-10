9119c10bbdf69855b9b29dc57275df0a57ad5f1f
akshaypandhare <akstpandhare@gmail.com>
2023-07-10 12:40:17 +0530

    Added new filea

--- dir1/python/mian.tf
+++ dir1/python/mian.tf
@@ -1,2 +1,2 @@
-elfnqlkwfmwefnoweifnewnfoinfn;
+wefnelfnqlkwfmwefnoweifnewnfoinfn;
 wefnlw
--- dire2/java/main.tf
+++ dire2/java/main.tf
@@ -1,18 +1,1 @@
-wefnwlsefnwlefknwenfkjwname: Check Updated Directories
-
-on:
-  push:
-    branches:
-      - master
-
-jobs:
-  check-updated-dirs:
-    runs-on: ubuntu-latest
-    
-    steps:
-    - name: Checkout code
-      uses: actions/checkout@v3
-      with:
-        fetch-depth: 0
-    - run: |
-            git show --format="" --name-only | xargs dirname | sort | uniqonerfoinwNkjenfjnefolinqpifn
+wefnwlfknwefnwlsefnwlefknwenfkjwname: Check Updated Directories
